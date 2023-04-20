89720ab47ee43a838772b96a9a53d72b966fe60e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-20 09:41:22 +0900

    feat: ScannerEX2 추가

---
+++ src/main/java/javaproject2/week1/ScannerEX2.java
@@ -0,0 +1,10 @@
+package javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerEX2 {
+    public void inputTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/javaproject2/week1/ScannerEX2Test.java
@@ -0,0 +1,8 @@
+package javaproject2.week1;
+
+public class ScannerEX2Test {
+    public static void main(String[] args) {
+        ScannerEX2 scannerEX2 = new ScannerEX2();
+        scannerEX2.inputTwoNumbersAndPlus();
+    }
+}
