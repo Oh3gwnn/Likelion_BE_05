f24f287aba048d366bb984fac3c4e73c29df933a
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-20 11:53:24 +0900

    feat: codeup 1011

---
+++ src/main/java/javaproject2/codeup/CodeUp11to20.java
@@ -0,0 +1,19 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp11to20 {
+    /* Main 클래스 복붙용
+    public class Main {
+        public static void main(String[] args) {
+
+        }
+    }
+    */
+    public static void main(String[] args) {
+        // 1011 (주의: import Scanner 넣어야됨)
+        Scanner sc = new Scanner(System.in);
+        char A = sc.next().charAt(0);
+        System.out.printf("%c", A);
+    }
+}
