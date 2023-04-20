605d8491ce594a5dcd2a271fc8cfb6ca5f573637
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-20 15:14:00 +0900

    feat: VariableEX

---
+++ src/main/java/javaproject2/week1/VariableEX.java
@@ -0,0 +1,16 @@
+package javaproject2.week1;
+
+import javaproject2.week0.PrintHello;
+
+public class VariableEX {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+
+        PrintHello printHello;
+        printHello = new PrintHello();
+
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
