b581eb1fed92273c0a6845f2df1f00a29d7b73d3
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-20 09:44:08 +0900

    feat: ScannerEX2 추가  - 문자열 합

--- src/main/java/javaproject2/week1/ScannerEX2.java
+++ src/main/java/javaproject2/week1/ScannerEX2.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class ScannerEX2 {
     public void inputTwoNumbersAndPlus() {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() + sc.nextInt());
+        System.out.println(sc.next() + sc.next());
     }
 }
