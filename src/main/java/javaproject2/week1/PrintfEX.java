216764f96950a05ef5e678c3150b33348ccf8010
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-20 11:18:42 +0900

    feat: printf 예제

---
+++ src/main/java/javaproject2/week1/PrintfEX.java
@@ -0,0 +1,12 @@
+package javaproject2.week1;
+
+public class PrintfEX {
+    void print() {
+        System.out.printf("%s", "Hi");
+    }
+
+    public static void main(String[] args) {
+        PrintfEX printfEX = new PrintfEX();
+        printfEX.print();
+    }
+}
