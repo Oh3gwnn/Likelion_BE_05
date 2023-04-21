1f1ae87cf000433e592986b2f3f2f4689b75db05
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-21 14:15:57 +0900

    feat: 멤버변수 예제

--- src/main/java/javaproject2/week1/SpaceInvadersMemberVariable.java
+++ src/main/java/javaproject2/week1/SpaceInvadersMemberVariable.java
@@ -5,18 +5,28 @@ public class SpaceInvadersMemberVariable {
 
     public void moveLeft() {
         location -= 1;
+    }
+    public void moveLeftAndPrint() {
+        location -= 1;
         System.out.printf("moveLeft: %d\n", location);
     }
+
     public void moveLight() {
         location += 1;
-        System.out.printf("moveRight: %d\n", location);
+    }
+
+    public void moveRightAndPrint() {
+        location += 1;
+        System.out.printf("moveLeft: %d\n", location);
     }
 
     public static void main(String[] args) {
         SpaceInvadersMemberVariable simv = new SpaceInvadersMemberVariable();
         simv.moveLeft();
-        simv.moveLeft();
         simv.moveLight();
         simv.moveLight();
+        simv.moveLight();
+        simv.moveLight();
+        System.out.printf("location: %d", simv.location);
     }
 }
