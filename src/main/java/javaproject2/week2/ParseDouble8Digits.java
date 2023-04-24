730bd2f6ffe095ec1fb5747da984216e8ce0d609
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-24 13:49:30 +0900

    feat: Float, Double Parsing(8Digits)

--- src/main/java/javaproject2/week2/ParseDouble8Digits.java
+++ src/main/java/javaproject2/week2/ParseDouble8Digits.java
@@ -1,6 +1,6 @@
 package javaproject2.week2;
 
-public class FloatPlus {
+public class ParseDouble8Digits {
     public static void main(String[] args) {
         String val1 = "1.11111111";
         String val2 = "2.11111111";
@@ -9,12 +9,5 @@ public class FloatPlus {
 
         double result2 = Double.parseDouble(val1) + Double.parseDouble(val2);
         System.out.println("double = " + result2);
-
-        // NumberFormatException 오류
-//        String val3 = "1.5";
-//        String val4 = "2.5";
-//
-//        int result3 = Integer.parseInt(val3) + Integer.parseInt(val4);
-//        System.out.println("result = " + result3);
     }
 }
