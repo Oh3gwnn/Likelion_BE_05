0e01f7b3819dda4da170d7e187cdaa25c5bde1f9
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-24 13:47:18 +0900

    feat: Float Parsing  - float, double  - string → int?

--- src/main/java/javaproject2/week2/FloatPlus.java
+++ src/main/java/javaproject2/week2/FloatPlus.java
@@ -9,5 +9,12 @@ public class FloatPlus {
 
         double result2 = Double.parseDouble(val1) + Double.parseDouble(val2);
         System.out.println("double = " + result2);
+
+        // NumberFormatException 오류
+//        String val3 = "1.5";
+//        String val4 = "2.5";
+//
+//        int result3 = Integer.parseInt(val3) + Integer.parseInt(val4);
+//        System.out.println("result = " + result3);
     }
 }
