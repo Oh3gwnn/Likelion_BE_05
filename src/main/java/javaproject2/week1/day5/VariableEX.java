ca9d61ba739fb8c75f78c201899e2b23e56ba2a9
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-24 10:05:31 +0900

    feat: 3 types

--- src/main/java/javaproject2/week1/day5/VariableEX.java
+++ src/main/java/javaproject2/week1/day5/VariableEX.java
@@ -1,16 +1,10 @@
 package javaproject2.week1.day5;
 
-import javaproject2.week0.PrintHello;
-
 public class VariableEX {
     public static void main(String[] args) {
-        int iVal;
-        iVal = 0;
-
-        PrintHello printHello;
-        printHello = new PrintHello();
-
-        System.out.println(iVal);
-        printHello.print();
+        String sOne = "1";
+        int iOne = 1;
+        float fOne = 1.0f;
+        System.out.printf("String:%s Number:%d Float:%f", sOne, iOne, fOne);
     }
 }
