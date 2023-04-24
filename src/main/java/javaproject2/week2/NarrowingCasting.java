a1b1fb4453d71d29b4656fa33e6625fcc5dae492
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-24 13:43:21 +0900

    feat: Float Parsing

---
+++ src/main/java/javaproject2/week2/NarrowingCasting.java
@@ -0,0 +1,40 @@
+package javaproject2.week2;
+
+public class NarrowingCasting {
+    public static void main(String[] args) {
+
+        // 112
+        System.out.println(1 + "1");
+        // int result = 1 + "1"; 컴파일 오류
+        // int result = 1 + Integer.parseInt("1"); 맞게 작성하기
+
+        // 113
+        double dVal = 123.456;
+        int iVal = (int) dVal;
+
+        System.out.println("dVal = " + dVal);
+        System.out.println("iVal = " + iVal);
+
+        // 114
+        String str = "1";
+        int num = Integer.parseInt(str);
+        System.out.println("num = " + num);
+
+        // 숫자가 아닌 문자열은 안됨
+        // NumberFormatException 발생
+        // String str = "123ab";
+        // int num = Integer.parseInt(str);
+
+        // 115
+        // String + String?
+        String val1 = "1.5";
+        String val2 = "2.5";
+        System.out.println("val1 + val2 = " + val1 + val2);
+
+        // float 타입으로 변환한 String + String
+        String val3 = "1.5";
+        String val4 = "2.5";
+        float result = Float.parseFloat(val3) + Float.parseFloat(val4);
+        System.out.println("val3 + val4 = " + result);
+    }
+}
