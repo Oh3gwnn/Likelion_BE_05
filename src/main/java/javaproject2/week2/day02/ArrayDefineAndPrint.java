904699717989b92909d7ed084f4c1f03cf6a1e97
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-25 14:32:52 +0900

    feat: Add ArrayDefineAndPrint  - fix StringSplit

---
+++ src/main/java/javaproject2/week2/day02/ArrayDefineAndPrint.java
@@ -0,0 +1,10 @@
+package javaproject2.week2.day02;
+
+import java.util.Arrays;
+
+public class ArrayDefineAndPrint {
+    public static void main(String[] args) {
+        int[] iArr = new int[]{1, 2, 3, 8}; // iArr length: 4
+        System.out.println(Arrays.toString(iArr));
+    }
+}
--- src/main/java/javaproject2/week2/day02/StringSplit.java
+++ src/main/java/javaproject2/week2/day02/StringSplit.java
@@ -1,15 +1,20 @@
 package javaproject2.week2.day02;
 
+import java.util.Arrays;
+
 public class StringSplit {
     public static void main(String[] args) {
         String str = "10 3 19 28 7 488";
 
         String[] strArr = str.split(" ");
         for (int i = 0; i < strArr.length; i++) System.out.printf("%s ", strArr[i]);
-
         System.out.printf("\n");
 
         String strArr2[] = str.split(" ");
         for (int i = 0; i < strArr.length; i++) System.out.printf("%s ", strArr2[i]);
+        System.out.printf("\n");
+
+        System.out.println(Arrays.asList(strArr2)); // string 배열 출력하기
+
     }
 }
