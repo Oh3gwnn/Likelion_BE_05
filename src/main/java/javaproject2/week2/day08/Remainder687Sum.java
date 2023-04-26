48312b1c70f299a174456d0ab0b96b714a1d3257
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-26 15:15:24 +0900

    feat: Remainder687Sum

---
+++ src/main/java/javaproject2/week2/day08/Remainder687Sum.java
@@ -0,0 +1,16 @@
+package javaproject2.week2.day08;
+
+import java.util.Arrays;
+
+public class Remainder687Sum {
+    public static void main(String[] args) {
+        int num = 687;
+        int a = num % 10;
+        num = num / 10;
+        int b = num % 10;
+        num = num / 10;
+        int c = num % 10;
+        System.out.println(a + b + c);
+    }
+}
+
