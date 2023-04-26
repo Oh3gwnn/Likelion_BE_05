e6988639821b0d74632813d73a00b6a9d4ed9865
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-26 13:08:52 +0900

    feat: Add TwoDimArrFillCross

--- src/main/java/javaproject2/week2/day08/TwoDimArrFillCross.java
+++ src/main/java/javaproject2/week2/day08/TwoDimArrFillCross.java
@@ -21,6 +21,6 @@ public class TwoDimArrFillCross {
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
         System.out.println(Arrays.toString(arr[3]));
-        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[4]));
     }
 }
