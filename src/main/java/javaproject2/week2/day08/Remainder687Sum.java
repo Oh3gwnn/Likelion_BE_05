09477db12ccfb3a6009665036775cc8a40c107d5
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-27 09:26:49 +0900

    feat: 687 예제, ArraySum

--- src/main/java/javaproject2/week2/day08/Remainder687Sum.java
+++ src/main/java/javaproject2/week2/day08/Remainder687Sum.java
@@ -4,10 +4,13 @@ public class Remainder687Sum {
     public static void main(String[] args) {
         int num = 687;
         int a = num % 10;
+
         num = num / 10;
         int b = num % 10;
+
         num = num / 10;
         int c = num % 10;
+
         System.out.println(a + b + c);
     }
 }
