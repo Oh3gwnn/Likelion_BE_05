06adbd27e303a7b3c3cfb890df2fe57c8a07af6e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-28 13:19:27 +0900

    feat: Programmers: 직사각형 별찍기

---
+++ src/main/java/javaproject2/programmers/RectangularStar.java
@@ -0,0 +1,16 @@
+package javaproject2.programmers;
+
+import java.util.Scanner;
+
+public class RectangularStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        for (int i = 0; i < b; i++) System.out.println("*".repeat(a));
+    }
+}
+
+/** 파이썬 풀이
+ * a, b = map(int, input().strip().split(' '))
+ * [print("*"*a) for _ in range(b)]
+ * **/
