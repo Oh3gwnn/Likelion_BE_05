0e64fed62ef96ffd2fff7679821be04eb033e04d
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-28 14:16:09 +0900

    feat: CodeUp1119~1121

---
+++ src/main/java/javaproject2/codeup/CodeUp1119.java
@@ -0,0 +1,11 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1119 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("%d", sc.nextInt()*24);
+    }
+}
---
+++ src/main/java/javaproject2/codeup/CodeUp1121.java
@@ -0,0 +1,11 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1121 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.printf("%d", a%b);
+    }
+}
