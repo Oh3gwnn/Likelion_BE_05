2b095138c0b380128bb04e6345564ce3307328bb
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-28 15:09:40 +0900

    feat: CodeUp1122~1124

---
+++ src/main/java/javaproject2/codeup/CodeUp1123.java
@@ -0,0 +1,11 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1123 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.printf("%.3f", (1.8 * a) + 32);
+    }
+}
