cc0ff4736cfb7dccff650f0cea21e934d60d1cdc
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-01 14:40:03 +0900

    feat: fix MultiplicationTable

--- src/main/java/javaproject2/week3/day11/MultiplicationTable.java
+++ src/main/java/javaproject2/week3/day11/MultiplicationTable.java
@@ -6,5 +6,12 @@ public class MultiplicationTable {
             for (int j = 1; j <= 9; j++) System.out.printf("[%d X %d = %2d] ", i, j, i*j);
             System.out.println();
         }
+        System.out.println();
+
+        int arr[] = {2, 4, 8, 9}; // for문 서로 바꾸면 가로 <-> 세로 바뀜
+        for (int a : arr) {
+            for (int i = 2; i <= 9; i++) System.out.printf("[%d X %d = %2d] ", a, i, a * i);
+            System.out.println();
+        }
     }
 }
