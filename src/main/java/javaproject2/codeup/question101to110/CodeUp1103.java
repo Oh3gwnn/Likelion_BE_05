2379bc5957a84c4686015e41ed45be7c47bc46eb
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-01 09:11:05 +0900

    feat: CodeUp 1103, 1106

--- src/main/java/javaproject2/codeup/question101to110/CodeUp1103.java
+++ src/main/java/javaproject2/codeup/question101to110/CodeUp1103.java
@@ -1,7 +1,5 @@
 package javaproject2.codeup.question101to110;
 
 public class CodeUp1103 {
-    public static void main(String[] args) {
-        System.out.println("\"c:\\test\"");
-    }
+    public static void main(String[] args) { System.out.println("\"c:\\test\""); }
 }
--- src/main/java/javaproject2/codeup/question101to110/CodeUp1106.java
+++ src/main/java/javaproject2/codeup/question101to110/CodeUp1106.java
@@ -1,7 +1,5 @@
 package javaproject2.codeup.question101to110;
 
 public class CodeUp1106 {
-    public static void main(String[] args) {
-        System.out.printf("%d %d", Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
+    public static void main(String[] args) { System.out.printf("%d %d", Integer.MIN_VALUE, Integer.MAX_VALUE); }
 }
