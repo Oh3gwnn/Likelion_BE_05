36eedb91a31680ec246cc450d19b8958258c1ccb
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-02 14:28:10 +0900

    feat: add WhileCountFactorial  - while) cnt 가로 출력  - while) factorial 출력

--- src/main/java/javaproject2/week3/day12/WhileCountFactorial.java
+++ src/main/java/javaproject2/week3/day12/WhileCountFactorial.java
@@ -1,13 +1,18 @@
 package javaproject2.week3.day12;
 
-public class WhileFactorial {
+public class WhileCountFactorial {
     public static void main(String[] args) {
+        // count 가로 출력
+        int cnt = 0;
+        while (cnt < 11) System.out.printf("%d ", cnt++);
+        System.out.println();
+
+        // factorial
         int num = 5;
         int answer = 1;
         while (num > 0){
             answer *= num--;
             System.out.printf("%d ", answer); // 가로 출력
         }
-        // System.out.println(answer); // 한 번 출력
     }
 }
