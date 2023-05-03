a10bb4c3505e18926bdfb7b22f26a792374fdb70
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-03 10:42:48 +0900

    feat: add IsPrime

---
+++ src/main/java/javaproject2/week3/day13/IsPrime.java
@@ -0,0 +1,19 @@
+package javaproject2.week3.day13;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        int num = 7;
+        for (int i = 2; i < num; i++) {
+            System.out.printf("%d %% %d = %d\n", num, i, num % i);
+        }
+
+        int factors = 0;
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0){
+                factors++;
+            }
+        }
+        System.out.println("factors: " + factors);
+        System.out.println(factors == 0);
+    }
+}
