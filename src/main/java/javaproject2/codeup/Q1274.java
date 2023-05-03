715e7104ef2b2adfb9d0963bc4865e2aee04e5d7
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-03 13:23:36 +0900

    feat: CodeUp 1274

---
+++ src/main/java/javaproject2/codeup/Q1274.java
@@ -0,0 +1,21 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Q1274 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 3) System.out.println("prime");
+        else{
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if(n % i == 0){
+                    n = -1;
+                    break;
+                }
+            }
+            if (n == -1) System.out.println("not prime");
+            else System.out.println("prime");
+        }
+    }
+}
