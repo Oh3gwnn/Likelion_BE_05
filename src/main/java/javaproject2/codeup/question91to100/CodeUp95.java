4978c579684a5f4dc235f09fbe89d6a03063b3cd
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-04 10:47:39 +0900

    feat: fix CodeUp 1094 1095  - 코드 바뀌어 있었음

--- src/main/java/javaproject2/codeup/question91to100/CodeUp95.java
+++ src/main/java/javaproject2/codeup/question91to100/CodeUp95.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class CodeUp95 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), a[] = new int[n];
-        for (int i = 0; i < n; i++) a[i] += sc.nextInt();
-        for (int i = n-1; i >= 0; i--) System.out.printf("%d ", a[i]);
+        int n = sc.nextInt(), arr[] = new int[n], compare = n;
+        for (int i = 0; i < n; i++) arr[i] += sc.nextInt();
+        for (int i = n-1; i >= 0; i--) compare = compare < arr[i] ? compare : arr[i];
+        System.out.println(compare);
     }
 }
