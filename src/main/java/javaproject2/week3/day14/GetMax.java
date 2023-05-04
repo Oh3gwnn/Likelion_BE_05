c0b75143d1f5911ffcbb006aa20cee729889e37c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-04 13:17:22 +0900

    feat: 2X2 최댓값

--- src/main/java/javaproject2/week3/day14/GetMax.java
+++ src/main/java/javaproject2/week3/day14/GetMax.java
@@ -1,7 +1,10 @@
 package javaproject2.week3.day14;
 
+import java.util.Scanner;
+
 public class GetMax {
     public static void main(String[] args) {
+        // 최댓값 구하기
         int[] arr = {2, 1, 31, 9, 7};
         int targetValue = arr[0];
         for(int i = 1; i < arr.length; i++) {
@@ -11,14 +14,15 @@ public class GetMax {
         }
         System.out.println(targetValue);
 
-        targetValue = arr[0];
-        for(int i = 1; i < arr.length; i++) {
-            if(targetValue > arr[i]) {
-                targetValue = arr[i];
+        // 2X2 배열 최댓값
+        Scanner sc = new Scanner(System.in);
+        int arr2[][] = {{1, 2}, {3, 4}};
+        int max = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (max < arr2[i][j]) max = arr2[i][j];
             }
-        }
-        System.out.println(targetValue);
+        } System.out.println(max);
     }
-
-
 }
