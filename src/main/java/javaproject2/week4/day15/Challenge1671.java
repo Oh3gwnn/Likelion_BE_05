7e55ff98ca1dc19a21bcf4f712afceb930cd8b1d
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-08 15:37:02 +0900

    feat: [도전] 랜덤 가위바위보 수정

--- src/main/java/javaproject2/week4/day15/Challenge1671.java
+++ src/main/java/javaproject2/week4/day15/Challenge1671.java
@@ -1,25 +1,40 @@
 package javaproject2.week4.day15;
 
+import java.util.Scanner;
+
 public class Challenge1671 {
-    public String Play(int user, int com) {
-        if (user == com) return "tie";
-        else if ((user==0 && com == 2) || (user==1 && com == 0) || (user==2 && com == 1)){
+    public String Play(String user, int com) {
+        int use = 0;
+        switch (user) {
+            case "가위": use = 1;
+            case "바위": use = 0;
+            case "보": use = 2;
+        }
+        if (use == com) return "tie";
+        else if ((use==0 && com == 2) || (use==1 && com == 0) || (use==2 && com == 1)){
             return "lose";
         } else return "win";
     }
 
     public static void main(String[] args) {
         Challenge1671 q1671 = new Challenge1671();
-        int user = 0, com = 0, cnt = 0;
+        Scanner sc = new Scanner(System.in);
+        String user = "";
+        int com = 0, cnt = 0;
 
         while (true) {
+            System.out.printf("가위, 바위, 보 : ");
             cnt += 1;
-            user = (int)(Math.random() * 2);
-            com = (int)(Math.random() * 2);
+            user = sc.nextLine();
+            com = (int)(Math.random() * 3);
             if (q1671.Play(user, com) == "win") {
                 System.out.printf("%d번 가위바위보 후 이겼습니다.", cnt);
                 break;
             }
+            else if (q1671.Play(user, com) == "tie") {
+                System.out.println("비겼습니다.");
+            }
+            else System.out.println("졌습니다.");
         }
     }
 }
