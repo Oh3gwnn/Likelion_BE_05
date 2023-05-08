ea356b8ee64f5fd4e6fbc07e29e6407cf091f2f1
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-08 15:49:15 +0900

    feat: add ArgsPrinter

---
+++ src/main/java/javaproject2/week4/day15/ArgsPrinter.java
@@ -0,0 +1,9 @@
+package javaproject2.week4.day15;
+
+import java.util.Arrays;
+
+public class ArgsPrinter {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
