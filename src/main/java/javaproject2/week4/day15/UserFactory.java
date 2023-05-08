af8de52e8b6a8e20be5bd8ea9ce52dbbaee781da
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-08 13:41:51 +0900

    feat: add UserFactory

---
+++ src/main/java/javaproject2/week4/day15/UserFactory.java
@@ -0,0 +1,9 @@
+package javaproject2.week4.day15;
+
+public class UserFactory {
+    public User getAdultUser() {
+        User user = new User();
+        user.age = 34;
+        return user;
+    }
+}
