19cde2a4e37cc57b02bbccc0ec4157977b173592
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-08 11:52:54 +0900

    feat: SpaceInvaders

---
+++ src/main/java/javaproject2/week4/day15/SpaceInvaders.java
@@ -0,0 +1,12 @@
+package javaproject2.week4.day15;
+
+public class SpaceInvaders {
+    int location;
+    private void moveLeft() {
+        location = location - 1;
+    }
+    private void moveRight() {
+        location = location + 1;
+    }
+}
+
---
+++ src/test/java/javaproject2/week4/day15/SpaceInvadersTest.java
@@ -0,0 +1,7 @@
+package javaproject2.week4.day15;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpaceInvadersTest {
+
+}
