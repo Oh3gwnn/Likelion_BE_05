158df065072654e721d53672428e690b6788c4d0
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-08 09:38:17 +0900

    feat: CodeUP 1256, 1287, 1362, 1367

---
+++ src/main/java/javaproject2/codeup/Q1256.java
@@ -0,0 +1,11 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Q1256 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) System.out.print("*");
+    }
+}
---
+++ src/main/java/javaproject2/codeup/Q1287.java
@@ -0,0 +1,14 @@
+package javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Q1287 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < n * i; j++) System.out.printf("*");
+            System.out.println();
+        }
+    }
+}
