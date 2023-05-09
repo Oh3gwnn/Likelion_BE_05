5c68cd2834223f2f21cd0497e81e5b4313c0c63f
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-09 11:28:53 +0900

    feat: add ReversePyramid

---
+++ src/main/java/javaproject2/week4/day16/ReversePyramid.java
@@ -0,0 +1,22 @@
+package javaproject2.week4.day16;
+
+public class ReversePyramid {
+    private String SpaceChar = "0";
+
+    public ReversePyramid(String spaceChar) {
+        this.SpaceChar = spaceChar;
+    }
+
+    public String makeALine(int height, int i) {
+        return String.format("%s %s\n", SpaceChar.repeat(i), 2 * (height - i) - 1);
+    }
+
+    public static void main(String[] args) {
+        ReversePyramid rp = new ReversePyramid(" ");
+        int height = 4;
+
+        for (int i = 0; i < height; i++) {
+            System.out.printf(rp.makeALine(height, i));
+        }
+    }
+}
