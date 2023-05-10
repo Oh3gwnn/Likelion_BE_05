0fb46f6191ef6c09015087afabc19eb8a9520ce3
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-10 10:15:52 +0900

    feat: fix DrawDiamond(도전 과제 두 가지) - output.txt: 파일 만들기 출력물

--- src/main/java/javaproject2/week4/day17/DrawDiamond.java
+++ src/main/java/javaproject2/week4/day17/DrawDiamond.java
@@ -1,5 +1,8 @@
 package javaproject2.week4.day17;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DrawDiamond {
     public static String getRepeatedSymbol(String symbol, int n) {
         return symbol.repeat(n);
@@ -15,15 +18,20 @@ public class DrawDiamond {
         return sb.toString();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 파일 만들기 (FileWriter)
+        FileWriter writer = new FileWriter("output.txt");
         int h = 7;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
             if(i <= pivot){
                 System.out.printf("%s%s\n", getRepeatedSymbol("0", -2 + h - i - 2), getRepeatedSymbol("*", 2 * i + 1));
+                writer.write(getRepeatedSymbol("0", -2 + h - i - 2) + getRepeatedSymbol("*", 2 * i + 1) + "\n");
             } else {
                 System.out.printf("%s%s\n", getStringBuilder("0", i - pivot), getStringBuilder("*", 2 * (h - i) - 1));
+                writer.write(getStringBuilder("0", i - pivot) + getStringBuilder("*", 2 * (h - i) - 1) + "\n");
             }
         }
+        writer.close();
     }
 }
