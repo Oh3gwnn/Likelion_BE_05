f5a3ba025f5126bd5f3725bc95ac897d8fe50b63
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-10 11:37:51 +0900

    feat: 추상메소드 정리

--- src/main/java/javaproject2/week4/day17/PyramidShapeDrawer2.java
+++ src/main/java/javaproject2/week4/day17/PyramidShapeDrawer2.java
@@ -8,7 +8,7 @@ public class PyramidShapeDrawer2 extends ShapeDrawer2 {
     // 하위 클래스의 메소드가 상위 클래스의 동일한 메소드를 덮어버림
     @Override
     public String makeALine(int h, int i) {
-        return String.format("%s%s\n", " ".repeat(h - i - 1), "*".repeat(2 * i + 1));
+        return String.format("%s%s\n", getRepeatedSymbol(" ",h - i - 1), getStringBuilder("*",2 * i + 1));
     }
 
     public static void main(String[] args) throws IOException {
