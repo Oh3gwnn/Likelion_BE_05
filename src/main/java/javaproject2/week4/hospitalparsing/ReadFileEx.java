4da2f1dc725e8ec1c8ca264ef87a3f572014371c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-12 10:49:49 +0900

    feat: add Hospital Parse()

--- src/main/java/javaproject2/week4/hospitalparsing/ReadFileEx.java
+++ src/main/java/javaproject2/week4/hospitalparsing/ReadFileEx.java
@@ -1,28 +1,40 @@
 package javaproject2.week4.hospitalparsing;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReadFileEx {
     GetLines getlines;
     public ReadFileEx(GetLines getlines) { this.getlines = getlines; }
 
-    public void PrintFile(List<String> lines) {
+    public void PrintLines(List<String> lines) {
         for (int i = 0; i < 3; i++) System.out.println(lines.get(i));
     }
 
+    // getlines Interface
     public void SelcetReader(int num, String fileName) throws IOException {
         switch (num) {
-            case 1 -> PrintFile(getlines.BufferedReaderFile(fileName));
-            case 2 -> PrintFile(getlines.BufferedReader2File(fileName));
-            case 3 -> PrintFile(getlines.InputStreamReaderFile(fileName));
+            case 1 -> PrintLines(getlines.BufferedReaderFile(fileName));
+            case 2 -> PrintLines(getlines.BufferedReader2File(fileName));
+            case 3 -> PrintLines(getlines.InputStreamReaderFile(fileName));
         }
+    }
+
+    public Hospital parse(String str) {
+        System.out.println(str);
+        String[] splitted = str.split(",");
+        System.out.println(Arrays.toString(splitted));
+        Address address = new Address();
+        Hospital hospital = new Hospital("", "", address);
+
+        return hospital;
     }
 
     public static void main(String[] args) throws IOException {
         ReadFileEx rfe = new ReadFileEx(new ReaderFile());
         rfe.SelcetReader(1, "hospital_info_2210.csv");
-        rfe.SelcetReader(2, "hospital_info_2210.csv");
-        rfe.SelcetReader(3, "hospital_info_2210.csv");
+
+        rfe.parse(rfe.getlines.BufferedReaderFile("hospital_info_2210.csv").get(2));
     }
 }
