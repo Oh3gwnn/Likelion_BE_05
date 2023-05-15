bcee2ee9e56cf3d5c432cfcf864c6193a5e4f5a1
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-15 10:27:18 +0900

    feat: modify BubbleSort OOP

--- src/main/java/javaproject2/week5/day20/BubbleSort02.java
+++ src/main/java/javaproject2/week5/day20/BubbleSort02.java
@@ -1,11 +1,18 @@
 package javaproject2.week5.day20;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BubbleSort02 {
+    public Comparator<Integer> comparator;
+
+    public BubbleSort02(Comparator<Integer> comparator) {
+        this.comparator = comparator;
+    }
+
     private int[] sortARound(int[] arr, int untll) {
         for (int i = 0; i < untll; i++) {
-            if(arr[i] > arr[i + 1]) {
+            if(comparator.compare(arr[i], arr[i + 1]) > 0) {
                 int tmp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = tmp;
@@ -22,10 +29,14 @@ public class BubbleSort02 {
     }
 
     public static void main(String[] args) {
-        BubbleSort02 bs = new BubbleSort02();
+        BubbleSort02 bs = new BubbleSort02((o1, o2) -> o2 - o1);
+        BubbleSort02 bs2 = new BubbleSort02(Comparator.comparingInt(o -> o));
         int[] arr = {7, 2, 3 ,9 ,21 ,28, 11, 1};
-        bs.sort(arr);
 
+        arr = bs.sort(arr);
         System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = bs2.sort(arr);
+        System.out.println(Arrays.toString(arr2));
     }
 }
