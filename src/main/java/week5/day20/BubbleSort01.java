24d1acf52762584dfa8d0bc29a50c927909157e4
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-15 09:39:33 +0900

    feat: modify BubbleSort

--- src/main/java/week5/day20/BubbleSort01.java
+++ src/main/java/week5/day20/BubbleSort01.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 
 public class BubbleSort01 {
     public static void main(String[] args) {
-        int[] arr = {7, 3 ,9 ,21 ,28, 2};
+        int[] arr = {7, 2, 3 ,9 ,21 ,28, 11, 1};
 
         for (int i = 0; i < arr.length - 1; i++) {
-            if(arr[i] > arr[i + 1]){
-                int tmp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = tmp;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
             }
         }
 
