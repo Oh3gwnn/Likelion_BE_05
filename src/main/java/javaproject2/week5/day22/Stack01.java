340e0d5830c6cb9a144f51a23ed49aed954f5bc7
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-17 11:02:12 +0900

    feat: add Stack2

--- src/main/java/javaproject2/week5/day22/Stack01.java
+++ src/main/java/javaproject2/week5/day22/Stack01.java
@@ -1,6 +1,6 @@
 package javaproject2.week5.day22;
 
-import java.util.Arrays;
+import java.util.EmptyStackException;
 
 public class Stack01 {
     private int[] arr = new int[10];
@@ -8,14 +8,28 @@ public class Stack01 {
 
     public void push(int val) {
         this.arr[pointer++] = val;
-        System.out.println(Arrays.toString(arr));
-        System.out.println("pointer = " + pointer);
+        System.out.println(pointer);
+    }
+
+    public int pop() {
+        if (!isEmpty()) return arr[--pointer];
+        else throw new EmptyStackException();
+    }
+
+    public boolean isEmpty() {
+        return pointer == 0;
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new EmptyStackException();
+        return arr[pointer-1];
     }
 
     public static void main(String[] args) {
         Stack01 st = new Stack01();
-        st.push(10);
         st.push(20);
+        System.out.println(st.pop());
         st.push(30);
+        System.out.println(st.peek());
     }
 }
---
+++ src/main/java/javaproject2/week5/day22/Stack02.java
@@ -0,0 +1,60 @@
+package javaproject2.week5.day22;
+
+import java.util.Stack;
+
+public class Stack02 {
+    private boolean isNumber(char c) {
+        return ('0' <= c && c <= '9');
+    }
+
+    private int priOpr(char operater) {
+        int priority = 0;
+        switch (operater) {
+            case '*', '/', '%' -> priority = 1;
+            case '+', '-' -> priority = 0;
+        } return priority;
+    }
+
+    public String toPostfix(String infix) {
+        Stack<Character> s = new Stack<>(); // 연산자
+        String postfix = "";                // 피연산자 + 처리된 연산자
+
+        for (char c : infix.toCharArray()) {
+            // 1. '('
+//            System.out.printf("%s\n", postfix); // 확인용
+            if (c == '(') s.add(c);
+
+            // 2. ')'
+            else if (c == ')') {
+                while (s.peek() != '(') postfix += s.pop();
+                s.pop();
+            }
+
+            // 3. 0~9
+            else if (isNumber(c)) postfix += c;
+
+            // 4. 연산자
+            else {
+                while (!s.isEmpty() && priOpr(c) < priOpr(s.peek())) {
+                    postfix += s.pop();
+                }
+//                postfix += ' '; //숫자 자리 구분용
+                s.add(c);
+            }
+        }
+        while (!s.isEmpty()) {
+            postfix += s.pop();
+        }
+
+        return postfix;
+    }
+
+
+    public static void main(String[] args) {
+        Stack02 st = new Stack02();
+        String infix = "(5*7)-((3*(3+7)-5/3)/(6+2)+3)";
+        String postfix = st.toPostfix(infix);
+        System.out.println(postfix);
+        // 숫자 많이나오면 작동이 이상하게되는데 나중에 시간나면하고...
+    }
+}
