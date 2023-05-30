0eb31f9fde3d55f247b5b2053c61b69429982dfd
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-30 13:50:39 +0900

    feat: add TSNGameTest, TSNGame TDD

--- src/main/java/com/example/tutoopjdbc/out/TSNGame.java
+++ src/main/java/com/example/tutoopjdbc/out/TSNGame.java
@@ -2,11 +2,14 @@ package com.example.tutoopjdbc.out;
 
 public class TSNGame {
     private static String clapCount(int i) {
-        if (((i%10)%3 == 0) && ((i%10) != 0)) return "*";
-        else if (((i/10)%3 == 0) && ((i/10) != 0)) return "*";
+        if ((i%10%3 == 0) && (i%10 != 0)) return "*";
+        else if ((i/10%3 == 0) && (i/10 != 0)) return "*";
         else return "";
     }
     public static void game369() {
-        for (int i = 0; i < 100; i++) System.out.printf("%02d%s\n", i, clapCount(i));
+        for (int i = 1; i < 100; i++) {
+            System.out.printf("%02d%2s ", i, clapCount(i));
+            if ((i%10 == 0) && (i != 0)) System.out.println();
+        }
     }
 }
