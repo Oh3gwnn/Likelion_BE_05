565fd4e8e992ab2fb47ff1be0bdcb9b355205334
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-30 11:26:15 +0900

    refactor: LeafYear file

--- src/main/java/com/example/tutoopjdbc/out/LeafYear.java
+++ src/main/java/com/example/tutoopjdbc/out/LeafYear.java
@@ -1,13 +1,13 @@
 package com.example.tutoopjdbc.out;
 
-import java.util.Scanner;
-
 public class LeafYear {
-    public static void main(String[] args) {
-        addLeafYear(1700);
+
+    private static boolean isTrueLeafYear(int year) {
+        if ((year%4 == 0) && (year%100 != 0) || (year%400 == 0)) return true;
+        else return false;
     }
 
     public static void addLeafYear(int year) {
-        System.out.println((year%4 == 0) && (year%100 != 0) || (year%400 == 0)? year + "년은 윤년입니다." : year + "년은 윤년이 아닙니다.");
+        System.out.println(isTrueLeafYear(year) ? year + "년은 윤년입니다." : year + "년은 윤년이 아닙니다.");
     }
 }
