7fee77f3458d55d5ebf868f51e03a43208d37960
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-30 11:20:05 +0900

    feat: add LeafYearTest file

--- src/main/java/com/example/tutoopjdbc/out/LeafYear.java
+++ src/main/java/com/example/tutoopjdbc/out/LeafYear.java
@@ -2,10 +2,12 @@ package com.example.tutoopjdbc.out;
 
 import java.util.Scanner;
 
-public class Leafyear {
+public class LeafYear {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
+        addLeafYear(1700);
+    }
+
+    public static void addLeafYear(int year) {
         System.out.println((year%4 == 0) && (year%100 != 0) || (year%400 == 0)? year + "년은 윤년입니다." : year + "년은 윤년이 아닙니다.");
     }
 }
---
+++ src/test/java/com/example/tutoopjdbc/out/LeafYearTest.java
@@ -0,0 +1,11 @@
+package com.example.tutoopjdbc.out;
+
+import org.junit.jupiter.api.Test;
+
+class LeafYearTest {
+
+    @Test
+    void addLeafYear() {
+        LeafYear.addLeafYear(2004);
+    }
+}
