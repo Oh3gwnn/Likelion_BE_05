777c6d427ee4329d4b781cacc99c7d1dd749f759
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-30 13:44:20 +0900

    feat: add TSNGameTest, TSNGame TDD

--- src/main/java/com/example/tutoopjdbc/out/TSNGame.java
+++ src/main/java/com/example/tutoopjdbc/out/TSNGame.java
@@ -6,7 +6,7 @@ public class TSNGame {
         else if (((i/10)%3 == 0) && ((i/10) != 0)) return "*";
         else return "";
     }
-    public static void main(String[] args) {
+    public static void game369() {
         for (int i = 0; i < 100; i++) System.out.printf("%02d%s\n", i, clapCount(i));
     }
 }
---
+++ src/test/java/com/example/tutoopjdbc/out/TSNGameTest.java
@@ -0,0 +1,9 @@
+package com.example.tutoopjdbc.out;
+
+import org.junit.jupiter.api.Test;
+
+class TSNGameTest {
+
+    @Test
+    void game369() { TSNGame.game369(); }
+}
