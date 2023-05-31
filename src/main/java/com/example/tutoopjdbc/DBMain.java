a2537168718bdf0f5b13da5c6b7fd04b2062fdc8
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-05-31 14:38:01 +0900

    feat: id 자동 증가, 테이블 생성, 추가 분리

--- src/main/java/com/example/tutoopjdbc/DBMain.java
+++ src/main/java/com/example/tutoopjdbc/DBMain.java
@@ -1,7 +1,5 @@
 package com.example.tutoopjdbc;
 
-
-
 import com.example.tutoopjdbc.model.BaseDAO;
 
 import java.sql.*;
@@ -10,24 +8,39 @@ public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
         dbMain.initPerson();
-        dbMain.gatCodeName();
+        dbMain.insertPerson("leo");
+        dbMain.insertPerson("yui");
+    }
+    private int insertPerson(String name) {
+        int cnt = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
     }
 
     private void initPerson() {
+        String sql = """
+                create table person (
+                id integer primary key autoincrement,
+                name string
+                )
+                """;
         try {
             getConn();
             smt = conn.createStatement();
-            smt.setQueryTimeout(30);  // set timeout to 30 sec.
-
             smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'leo')");
-            smt.executeUpdate("insert into person values(2, 'yui')");
-            rs = smt.executeQuery("select * from person");
-            while(rs.next()) {
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
+            smt.executeUpdate(sql);
         }
         catch(SQLException e) {
             System.err.println(e.getMessage());
