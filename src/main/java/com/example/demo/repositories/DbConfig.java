cbbf432eed441b1c8958064de0085b700a027324
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-02 10:28:27 +0900

    initially commit

---
+++ src/main/java/com/example/demo/repositories/DbConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.demo.repositories")
+public class DbConfig {
+}
