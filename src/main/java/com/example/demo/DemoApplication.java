19999943b21c5d8f5aab564e8d8cdc0c81248ac8
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-02 13:20:05 +0900

    feat: log 기능 추가 - Bean 어노테이션

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,24 +2,30 @@ package com.example.demo;
 
 import com.example.demo.models.Customer;
 import com.example.demo.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+	// log
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello() {
-		return "hello";
+	// hello
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return "hello" + name;
 	}
 
 	@Bean
@@ -28,8 +34,9 @@ public class DemoApplication {
 			repository.save(new Customer("leo", "galil"));
 
 			for (Customer customer : repository.findAll()) {
-
+				log.info(customer.toString());
 			}
+			log.info("");
 		};
 	}
 }
