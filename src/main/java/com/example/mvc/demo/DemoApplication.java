b4ec0563781fc8bdd5d1dd7b01867eebe9a21d1e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-08 13:42:46 +0900

    modified: Demo2App... -> DemoApp...

--- src/main/java/com/example/mvc/demo/DemoApplication.java
+++ src/main/java/com/example/mvc/demo/DemoApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Demo2Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		// 현재 실행 중인 IoC Container 반환
 		// -> IoC Container는 클래스를 객체로 만들어 내부 관리
-		ApplicationContext applicationContext = SpringApplication.run(Demo2Application.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
 
 		// Container가 Bean 객체를 가지고 있는지 확인
 		for (String beanName: applicationContext.getBeanDefinitionNames()) {
