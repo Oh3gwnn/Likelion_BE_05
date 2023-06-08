0a004491f69178006c92849e934beef8b1f20060
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-08 13:43:20 +0900

    feat: html form - add receive

--- src/main/java/com/example/mvc/form/FormController.java
+++ src/main/java/com/example/mvc/form/FormController.java
@@ -2,11 +2,21 @@ package com.example.mvc.form;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
     @RequestMapping("/send")
     public String send() {
+        return "form/send";
+    }
+
+    @RequestMapping("/receive")
+    public String receive(
+            @RequestParam("msg")
+            String msg
+    ) {
+        System.out.println(msg);
         return "send";
     }
 }
