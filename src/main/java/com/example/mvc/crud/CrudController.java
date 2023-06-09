33a35efad4d254dbc4bb2f1b620a574eb24da345
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-09 11:53:53 +0900

    feat: modified CrudController - @GetMapping("/{id}") -> 특정 아이디 변수 설정 - @PathVariable -> URL 경로에 변수를 넣어준다.

--- src/main/java/com/example/mvc/crud/CrudController.java
+++ src/main/java/com/example/mvc/crud/CrudController.java
@@ -4,6 +4,7 @@ import com.example.mvc.model.StudentDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,8 +45,9 @@ public class CrudController {
         return "crud/crudhome";
     }
 
-    @GetMapping("/read")
-    public String read(Long id, Model model) {
+    @GetMapping("/{id}") // {} <- 특정 아이디를 가져오게끔 설정
+    public String read(@PathVariable("id") Long id, Model model) {
+        // @PathVariable - URL 경로에 변수를 넣어준다.
         model.addAttribute(
                 "student",
                 studentService.readStudent(id)
