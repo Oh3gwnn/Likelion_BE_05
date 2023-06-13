6115bef9f2831896c64b6a0183c404f1948a1d9d
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-13 15:27:47 +0900

    updated: StudentXmlMapper  - selectStudentAll

--- src/main/java/com/example/mybatis/mapper/StudentXmlMapper.java
+++ src/main/java/com/example/mybatis/mapper/StudentXmlMapper.java
@@ -1,4 +1,9 @@
 package com.example.mybatis.mapper;
 
+import com.example.mybatis.model.Student;
+
+import java.util.List;
+
 public interface StudentXmlMapper {
+    List<Student> selectStudentAll();
 }
