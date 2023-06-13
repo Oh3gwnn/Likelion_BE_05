65698707755516d19438308785f4d103008f86a0
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-13 15:31:07 +0900

    feat: DB initialization Code

--- dml.sql
+++ dml.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS students;
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    name TEXT,
+    age INTEGER,
+    phone TEXT,
+    email TEXT
+);
+
 INSERT INTO students (name, age, phone, email)
 VALUES ('alex', 35, '010-1234-5678', 'alex@gmail.com');
 
@@ -6,3 +15,5 @@ VALUES ('brad', 35, '010-1234-5678', 'brad@gmail.com');
 
 INSERT INTO students (name, age, phone, email)
 VALUES ('chad', 35, '010-1234-5678', 'chad@gmail.com');
+
+SELECT * FROM students;
