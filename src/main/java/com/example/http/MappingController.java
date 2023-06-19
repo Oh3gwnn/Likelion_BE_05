4dfe01580991ff8472876726d16829303bdf816f
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-19 14:32:42 +0900

    add: RequestMapping(method) ex  - POST(consumes(Json), headers, params)

--- src/main/java/com/example/http/MappingController.java
+++ src/main/java/com/example/http/MappingController.java
@@ -3,6 +3,7 @@ package com.example.http;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,6 +39,39 @@ public class MappingController {
     @RequestMapping(value = "/path", method = RequestMethod.DELETE)
     public String DeletePath() {
         log.info("DELETE /path");
+        return "index";
+    }
+
+    // '/path' - POST(JSON 데이터 포함)
+    @RequestMapping(
+            value = "/path",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE // import org.springframework.http.MediaType;
+    )
+    public String consumes() {
+        log.info("POST /path Content-Type: application/json");
+        return "index";
+    }
+
+    // '/path' - POST(Header -> x-likelion=hello)
+    @RequestMapping(
+            value = "/path",
+            method = RequestMethod.POST,
+            headers = "x-likelion=hello"
+    )
+    public String headerWith() {
+        log.info("POST /path with x-likelion=hello");
+        return "index";
+    }
+
+    // '/path' - POST(Query Parameter -> x-likelion=hello)
+    @RequestMapping(
+            value = "/path",
+            method = RequestMethod.POST,
+            params = "x-likelion=hello"
+    )
+    public String params() {
+        log.info("POST /path with parameter likelion");
         return "index";
     }
 }
