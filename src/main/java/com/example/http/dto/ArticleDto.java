13265b45fb8e16d5e32232dbc219262db51dd028
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-19 15:37:23 +0900

    feat: add Dto(Article, Response)

---
+++ src/main/java/com/example/http/dto/ArticleDto.java
@@ -0,0 +1,9 @@
+package com.example.http.dto;
+
+import lombok.Data;
+
+@Data
+public class ArticleDto {
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/example/http/dto/ResponseDto.java
@@ -0,0 +1,9 @@
+package com.example.http.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseDto {
+    private Integer status;
+    private String message;
+}
