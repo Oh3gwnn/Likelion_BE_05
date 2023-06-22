4af0e8fbae188de104eca6f5f6948ac0f09c959e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-22 11:25:52 +0900

    todo: 게시글 댓글 전체 조회

--- src/main/java/com/example/article/CommentController.java
+++ src/main/java/com/example/article/CommentController.java
@@ -5,15 +5,27 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor @RequestMapping("/articles/{articleId}/comments")
 public class CommentController {
     private final CommentService service;
 
+    // TODO 게시글 작성
     @PostMapping
     public CommentDto create(@PathVariable("articleId") Long articleId,
                              @RequestBody CommentDto dto) {
         return service.createComment(articleId, dto);
     }
+
+    // TODO 게시글 댓글 전체 조회
+    @GetMapping
+    public List<CommentDto> readAll(@PathVariable("articleId") Long articleId) {
+        return service.readCommentAll(articleId);
+    }
+
+    // TODO 게시글 댓글 수정
+    // TODO 게시글 댓글 삭제
 }
