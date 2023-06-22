8c6d7691a4fbdf09e999b6a070860359a63e70b9
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-22 10:33:52 +0900

    feat: 전위 순회 이진트리

---
+++ src/main/java/javaproject2/week10/BinaryTree.java
@@ -0,0 +1,22 @@
+package javaproject2.week10;
+
+public class BinaryTree {
+    private int nodes;
+    private int[] arr;  // 이진 트리를 표현하기 위한 배열
+
+    // { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }
+    public void setArr(int[] arr) {
+        this.arr = arr;
+        this.nodes = arr.length;
+    }
+
+    // 전위 순회 V -> L -> R
+    // preorder(): System.out.print(V) -> preorder(L) -> preorder(R)
+    public void traversePreorder(int node) {
+        if (node < this.nodes && arr[node] != 0) {
+            System.out.print(arr[node] + ", ");   // 방문
+            this.traversePreorder(node * 2);      // 왼쪽 자식(i * 2)을 루트로 다시 preorder 호출
+            this.traversePreorder(node * 2 + 1);  // 오른쪽 자식(i * 2 + 1)을 루트로 다시 preorder 호출
+        }
+    }
+}
