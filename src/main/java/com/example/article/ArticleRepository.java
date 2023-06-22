c730f78a2324b488125f25c156c793f628a3f245
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-22 14:23:08 +0900

    feat: query 실습

--- src/main/java/com/example/article/ArticleRepository.java
+++ src/main/java/com/example/article/ArticleRepository.java
@@ -3,4 +3,10 @@ package com.example.article;
 import com.example.article.entity.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {}
+import java.util.List;
+
+public interface ArticleRepository
+        extends JpaRepository<ArticleEntity, Long> {
+    // ID 큰 순서대로 최상위 20개
+    List<ArticleEntity> findTop20ByOrderByIdDesc();
+}
---
+++ src/main/java/com/example/article/QueryController.java
@@ -0,0 +1,27 @@
+package com.example.article;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class QueryController {
+    // GET /path?query=keyword&limit=20
+    @GetMapping("/path")
+    public Map<String, Object> queryParams(
+            @RequestParam("query") String query,
+            @RequestParam(value = "limit", defaultValue = "10") String limit
+    ) {
+        log.info("query=" + query);
+        log.info("limit=" + limit);
+        Map<String, Object> response = new HashMap<>();
+        response.put("query", query);
+        response.put("limit", limit);
+        return response;
+    }
+}
