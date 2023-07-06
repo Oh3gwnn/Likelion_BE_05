bdceb53e22f9c023b94208758323ab1ff5c817eb
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-06 13:18:26 +0900

    feat: http.authorizeHttpRequests

--- src/main/java/com/example/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -12,7 +12,15 @@ import org.springframework.security.web.SecurityFilterChain;
 //@EnableWebSecurity -> 2.1 버전 이후 Spring BOot Starter Security 에서는 필수 X
 public class WebSecurityConfig {
     @Bean // 메서드의 결과를 Bean 객체로 등록
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http // DI 자동으로 설정됨, 빌더 패턴처럼 쓴다.
+            ) throws Exception {
+        // HTTP 요청 허가 관련 설정을 하고 싶다.
+        http.authorizeHttpRequests(authHttp -> authHttp
+                .requestMatchers("no-auth") // requestMatchers -> 어떤 URL로 오는 요청에 대하여 설명하는지
+                .permitAll() // permitAll -> 누가 요청해도 허락한다.
+        );
+
         return http.build();
     }
 }
