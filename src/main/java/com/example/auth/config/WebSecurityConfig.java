e97d8c3c8b0e0e25b05d164a6afbff942b6624fd
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-06 13:08:31 +0900

    feat: add WebSecurityConfig

--- src/main/java/com/example/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -3,9 +3,13 @@ package com.example.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 5.7 버전 이전: extends WebSecurityConfigurerAdapter
+// 6.1 버전 이후: Builder -> Lambda DSL 기반 설정
 @Configuration
+//@EnableWebSecurity -> 2.1 버전 이후 Spring BOot Starter Security 에서는 필수 X
 public class WebSecurityConfig {
     @Bean // 메서드의 결과를 Bean 객체로 등록
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
