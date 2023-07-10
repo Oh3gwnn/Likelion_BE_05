4d5fc53a1e5ee99a8cf4d805c43b4fe0f419a46c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-10 12:57:37 +0900

    feat: JWT initial set

--- src/main/java/com/example/auth/jwt/JwtTokenUtils.java
+++ src/main/java/com/example/auth/jwt/JwtTokenUtils.java
@@ -1,41 +1,23 @@
 package com.example.auth.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.sql.Date;
-import java.time.Instant;
 
 @Slf4j @Component
-// JWT 관련 기능들을 넣어두기 위한 기능성 클래스
+// JWT 생성, 인증 등의 기능을 가지고 있을 컴포넌트
 public class JwtTokenUtils {
+    // JWT는 암호화를 거쳐서 만들어지는데,
+    // 이를 위해서 암호키가 필요하다.
     private final Key signingKey;
 
-    // @Value -> lombok X, springframework O
-    public JwtTokenUtils(@Value("${jwt.secret}") String jwtSecret) {
-        this.signingKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
-    }
-
-    // 주어진 사용자 정보를 바탕을 JWT -> 문자열 생성
-    public String generateToken(UserDetails userDetails) {
-        // JWT에 담는 정보의 단위 -> Claim
-        // Claims -> Claim들을 담기 위한 Map의 상속 interface
-        Claims jwtClaims = Jwts.claims()
-                // 사용자 정보 등록 -> 1시간 동안 유효
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(Date.from(Instant.now().plusSeconds(3600)));
-
-        return Jwts.builder()
-                .setClaims(jwtClaims)
-                .signWith(signingKey)
-                .compact();
+    public JwtTokenUtils(
+            @Value("${jwt.secret}")
+            String jwtSecret
+    ) {
+        this.signingKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
     }
 }
